8f68a69383102bf1c9a6476cb38429ad8ceb986e
Tomáš Houfek <tomashoufek@seznam.cz>
2021-05-11 10:27:57 +0200

    Fixed mirax path in converter

--- convert.sh
+++ convert.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 FILE=$1
 
-DIR_WITH_MIRAX=/images
 DIR_WITH_TIFF=/tiff
 
 DIRNAME=`dirname "$FILE"`
@@ -9,8 +8,8 @@ BASENAME=`basename "$FILE"`
 FILENAME=${BASENAME%.mrxs}
 
 echo "---Converting  $BASENAME to $FILENAME.tiff---"
-python3 extract_label.py $DIR_WITH_MIRAX/$FILE
-vips tiffsave $DIR_WITH_MIRAX/$FILE $FILENAME.tiff --tile --pyramid --compression jpeg --Q 45 --tile-width 512 --tile-height 512 --bigtiff
+python3 extract_label.py $FILE
+vips tiffsave $FILE $FILENAME.tiff --tile --pyramid --compression jpeg --Q 45 --tile-width 512 --tile-height 512 --bigtiff
 mv $FILENAME.tiff $DIR_WITH_TIFF/$FILENAME.tiff
 mv $FILENAME-label.png $DIR_WITH_TIFF/$FILENAME-label.png
 
